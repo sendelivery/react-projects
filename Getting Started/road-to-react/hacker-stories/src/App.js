4bc493e3ba080fa805ce819c1b13ae00153f5345
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-08-10 12:42:44 +0100

    Custom hooks, fragments, and reusable components

--- Getting Started/road-to-react/hacker-stories/src/App.js
+++ Getting Started/road-to-react/hacker-stories/src/App.js
@@ -1,5 +1,28 @@
 import React from "react";
 
+// Hook naming convention: puts the 'use' prefix in front of every hook
+// Another convention, a hook's internals should be about a value being set and synchronised with local storage,
+// not a specific domain like Search
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    // Using local storage in React is a side-effect because we are
+    // interacting outside of React's domain using the browser's API.
+    localStorage.getItem(key) || initialState
+  );
+
+  // The useEffect hook takes two arguments:
+  // The first, is a function where the side effect occurs. (What we run)
+  // The optional second argument is a dependency array of variables. If one of theses variables changes,
+  // the function for the side-effect is called. This function also is called when the component renders for the first time.
+  // Leaving out the second arg would make the side-effect function run on every render, an empty array = only on initial render.
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  // Second convention, the returned values are returned as an array to allow for destructuring
+  return [value, setValue];
+}
+
 const App = () => {
   const stories = [
     {
@@ -20,20 +43,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    // Using local storage in React is a side-effect because we are
-    // interacting outside of React's domain using the browser's API.
-    localStorage.getItem("search") || "React"
-  );
-
-  // The useEffect hook takes two arguments:
-  // The first, is a function where the side effect occurs. 
-  // The optional second argument is a dependency array of variables. If one of theses variables changes,
-  // the function for the side-effect is called. This function also is called when the component renders for the first time.
-  // Leaving out the second arg would make the side-effect function run on every render, an empty array = only on initial render.
-  React.useEffect(() => {
-    localStorage.setItem('search', searchTerm);
-  }, [searchTerm]);
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -46,25 +56,32 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch} search={searchTerm}/>
+      {/* The specialised search component can be made more general and reusable by specifying attributes like so */}
+      <InputWithLabel 
+        id='search'
+        label='Search: '
+        value={searchTerm}
+        onInputChange={handleSearch} 
+      />
       <hr />
       <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = ({ search, onSearch }) =>
-  <div>
-    <label htmlFor="search">Search: </label>
+// If no type prop is passed, the default 'text' takes over. 
+const InputWithLabel = ({ id, label, type = 'text', value, onInputChange }) =>
+  <>
+    <label htmlFor={id}>{label}</label>
     <input 
-      id="search" 
-      type="text" 
-      value={search}
-      onChange={onSearch} />
+      id={id} 
+      type={type} 
+      value={value}
+      onChange={onInputChange} />
     <p>
-      Searching for: <strong>{search}</strong>
+      Searching for: <strong>{value}</strong>
     </p>
-  </div>
+  </>
 
 const List = ({ list }) => 
   list.map(item => <Item key={item.objectID} item={item} />);
