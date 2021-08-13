391f329f3f756b41c0f910c0937a4e61e75f61eb
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-08-13 23:21:11 +0100

    Image upload component

--- Getting Started/3 image-compressor-project/src/App.js
+++ Getting Started/3 image-compressor-project/src/App.js
@@ -1,19 +1,54 @@
-function App() {
-  return (
-    <div className="App">
-      <Instructions />
-    </div>
-  );
-}
+import React from "react";
+import "./App.css";
 
-const Instructions = () => 
+const App = () => {
+  const [image, setImage] = React.useState(null);
+
+  const handleChange = (event) => {
+    /* The URL.creatObjectURL() method takes an object (like our file) and creates a temporary local URL 
+       that is tied to the document in which it is created (meaning it won’t remember the URL if you leave 
+       the page and come back). This URL can be used to set the the src property of a <img/> element. */
+    setImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  return (
     <>
-      <h1>Three Simple Steps</h1>
-      <ol>
-        <li>Upload Image</li>
-        <li>Click on Compress</li>
-        <li>Download Compressed Image</li>
-      </ol>
+      <div className="App">
+        <Instructions />
+      </div>
+      <div className="key-section">
+        <Upload onUploadChange={handleChange} />
+        <img src={image} alt="User uploaded" />
+      </div>
+      <div className="key-section">
+        <Compress />
+      </div>
+      <div className="key-section">
+        <Download />
+      </div>
     </>
+  );
+};
+
+const Upload = ({ onUploadChange }) => (
+  <>
+    <input type="file" onChange={onUploadChange} accept="image/*" />
+  </>
+);
+
+const Compress = () => <>Compress</>;
+
+const Download = () => <>Download</>;
+
+const Instructions = () => (
+  <>
+    <h1>Three Simple Steps</h1>
+    <ol>
+      <li>Upload Image</li>
+      <li>Click on Compress</li>
+      <li>Download Compressed Image</li>
+    </ol>
+  </>
+);
 
 export default App;
