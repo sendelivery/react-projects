d3aa4e1a6b00b95b4eb35658bc100d51d6f4305b
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-08-16 21:28:58 +0100

    Download functionality

--- Getting Started/3 image-compressor-project/src/App.js
+++ Getting Started/3 image-compressor-project/src/App.js
@@ -3,6 +3,24 @@ import imageCompression from "browser-image-compression";
 import "./App.css";
 
 const App = () => {
+  const createImageObject = (image) => {
+    if (image === null) {
+      const emptyObj = {
+        name: "",
+        file: null,
+        url: null,
+      };
+      return emptyObj;
+    }
+
+    const imageObj = {
+      name: image.name,
+      file: image,
+      url: URL.createObjectURL(image),
+    };
+    return imageObj;
+  };
+
   const [options, setOptions] = React.useState(
     {
       maxSizeMB: 7,
@@ -10,40 +28,47 @@ const App = () => {
     }
   );
 
-  const [imageURL, setImageURL] = React.useState(null);
-  const [image, setImage] = React.useState(null);
-  const [compressedImage, setCompressedImage] = React.useState(null);
+  const [image, setImage] = React.useState();
+  const [compressedImage, setCompressedImage] = React.useState();
 
   const handleChange = (event) => {
+    const imageObj = createImageObject(event.target.files[0])
     /* The URL.creatObjectURL() method takes an object (like our file) and creates a temporary local URL 
        that is tied to the document in which it is created (meaning it won’t remember the URL if you leave 
-       the page and come back). This URL can be used to set the the src property of a <img/> element. */
-    setImageURL(URL.createObjectURL(event.target.files[0]));
-    setImage(event.target.files[0]);
-    console.log('input: ', event.target.files[0]);
+       the page and come back). */
+    
+    console.log("input: ", imageObj.file);
+    setImage(imageObj);
   };
 
   async function compressImage() {
-    const output = await imageCompression(image, options);
-    console.log('output: ', output);
-    setCompressedImage(URL.createObjectURL(output));
+    const output = await imageCompression(image.file, options);
+
+    const imageObj = createImageObject(output);
+
+    console.log('output: ', imageObj.file);
+    setCompressedImage(imageObj);
   }
 
   return (
     <>
-      <div className="App">
+      <div className="instructions">
         <Instructions />
       </div>
       <div className="key-section">
-        <img src={imageURL} alt="User uploaded" />
+        {image ? <img src={image.url} alt="User uploaded" /> : <></>}
         <Upload onUploadChange={handleChange} />
       </div>
       <div className="key-section">
         <Compress onCompressClick={compressImage} />
       </div>
       <div className="key-section">
-        <img src={compressedImage} alt="User uploaded" />
-        <Download />
+        {compressedImage ? (
+          <img src={compressedImage.url} alt="User uploaded" />
+        ) : (
+          <></>
+        )}
+        <Download image={compressedImage} />
       </div>
     </>
   );
@@ -61,13 +86,23 @@ const Compress = ({ onCompressClick }) => (
   </>
 );
 
-const Download = () => <>Download</>;
+const Download = ({ image }) => (
+  <>
+    {image ? (
+      <a href={image.url} download={image.name}>
+        Download
+      </a>
+    ) : (
+      <></>
+    )}
+  </>
+);
 
 const Instructions = () => (
   <>
-    <h1>Three Simple Steps</h1>
+    <h1 className="display-4">Three Simple Steps</h1>
     <ol>
-      <li>Upload Image</li>
+      <li className="font-weight-normal">Upload Image</li>
       <li>Click on Compress</li>
       <li>Download Compressed Image</li>
     </ol>
