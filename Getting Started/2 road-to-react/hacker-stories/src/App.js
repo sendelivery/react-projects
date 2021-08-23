ea25cc3ddf382dc0b144f8126745e76a845a0e2a
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-08-23 22:01:43 +0100

    Inline handlers

--- Getting Started/2 road-to-react/hacker-stories/src/App.js
+++ Getting Started/2 road-to-react/hacker-stories/src/App.js
@@ -1,5 +1,24 @@
 import React from "react";
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 // Hook naming convention: puts the 'use' prefix in front of every hook
 // Another convention, a hook's internals should be about a value being set and synchronised with local storage,
 // not a specific domain like Search
@@ -21,31 +40,22 @@ const useSemiPersistentState = (key, initialState) => {
 
   // Second convention, the returned values are returned as an array to allow for destructuring
   return [value, setValue];
-}
+};
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+  const [stories, setStories] = React.useState(initialStories);
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const handleRemoveStory = (item) => {
+    const newStory = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
 
-  const handleSearch = event => {
+    setStories(newStory);
+  };
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+
+  const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
@@ -56,30 +66,39 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <InputWithLabel 
-        id='search'
+      <InputWithLabel
+        id="search"
         value={searchTerm}
         isFocused={true}
-        onInputChange={handleSearch} 
+        onInputChange={handleSearch}
       >
-        <strong><Text /></strong> 
+        <strong>
+          <Text />
+        </strong>
       </InputWithLabel>
       <hr />
-      <List list={searchedStories} />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 };
 
-// If no type prop is passed, the default 'text' takes over. 
+// If no type prop is passed, the default 'text' takes over.
 // React components behave similarly to html tags, everything passed between tags are accessed in the children prop as below:
 // using that we can also compose components inside components, by passing them as React children.
-const InputWithLabel = ({ id, type = 'text', value, isFocused, onInputChange, children }) => {
+const InputWithLabel = ({
+  id,
+  type = "text",
+  value,
+  isFocused,
+  onInputChange,
+  children,
+}) => {
   // A
   const inputRef = React.useRef();
 
   // C
   React.useEffect(() => {
-    if (isFocused && inputRef.current) { 
+    if (isFocused && inputRef.current) {
       // D
       inputRef.current.focus();
     }
@@ -101,7 +120,7 @@ const InputWithLabel = ({ id, type = 'text', value, isFocused, onInputChange, ch
       </p>
     </>
   );
-}
+};
 
 /* 
   (A) First, create a ref with React’s useRef hook. This ref object is a persistent value which
@@ -116,23 +135,27 @@ const InputWithLabel = ({ id, type = 'text', value, isFocused, onInputChange, ch
   isFocused is set and the current property is existent.
 */
 
-const Text = () => 
-  <>
-    Search: 
-  </>
+const Text = () => <>Search:</>;
 
-const List = ({ list }) => 
-  list.map(item => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  ));
 
-const Item = ({ item }) => (
-  <div>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </div>
-)
+const Item = ({ item, onRemoveItem }) => (
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  );
 
 export default App;
