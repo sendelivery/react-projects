6957f5b2c2525da64180b017b882f878e910fdbe
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-09-05 00:57:14 +0100

    Cleanup

--- Getting Started/2 road-to-react/hacker-stories/src/App.js
+++ Getting Started/2 road-to-react/hacker-stories/src/App.js
@@ -2,29 +2,7 @@ import React from "react";
 // Substituting native fetch API with the axios library to ensure fetch works with older browsers.
 import axios from "axios";
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-  );
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -70,8 +48,6 @@ const storiesReducer = (state, action) => {
       throw new Error();
   }
 };
-
-const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "");
