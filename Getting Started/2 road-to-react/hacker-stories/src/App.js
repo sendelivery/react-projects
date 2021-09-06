1958c8c48f46f061e0cf0ce076c3125e880c404c
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-09-06 12:34:07 +0100

    Added CSS

--- Getting Started/2 road-to-react/hacker-stories/src/App.js
+++ Getting Started/2 road-to-react/hacker-stories/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 // Substituting native fetch API with the axios library to ensure fetch works with older browsers.
 import axios from "axios";
 
+import "./App.css";
+
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const useSemiPersistentState = (key, initialState) => {
@@ -119,8 +121,8 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -128,7 +130,6 @@ const App = () => {
         onSearchSubmit={handleSearchSubmit}
       />
 
-      <hr />
       {stories.isError && <p>Something went wrong...</p>}
 
       {stories.isLoading ? (
@@ -141,7 +142,7 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
     <InputWithLabel
       id="search"
       value={searchTerm}
@@ -151,7 +152,7 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
       <strong>Search: </strong>
     </InputWithLabel>
 
-    <button type="submit" disabled={!searchTerm}>
+    <button type="submit" disabled={!searchTerm} className="button_large">
       Submit
     </button>
   </form>
@@ -175,17 +176,17 @@ const InputWithLabel = ({
 
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       <input
         ref={inputRef}
         id={id}
         type={type}
         value={value}
         onChange={onInputChange}
+        className="input"
       />
-      <p>
-        Searching for: <strong>{value}</strong>
-      </p>
     </>
   );
 };
@@ -196,15 +197,19 @@ const List = ({ list, onRemoveItem }) =>
   ));
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <div className="item">
+    <span style={{ width: "40%" }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
+    <span style={{ width: "30%" }}>{item.author}</span>
+    <span style={{ width: "10%" }}>{item.num_comments}</span>
+    <span style={{ width: "10%" }}>{item.points}</span>
+    <span style={{ width: "10%" }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)}
+        className="button button_small"
+      >
         Dismiss
       </button>
     </span>
