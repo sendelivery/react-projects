39364d721b06f7ccfb931e7456b7aba98bbdc5ae
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-10-03 16:53:30 +0100

    Heading styles

--- React and Tailwind/1 tailwind-first-step/src/components/header.js
+++ React and Tailwind/1 tailwind-first-step/src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
     return (
       <div className="grid grid-cols-1 bg-gray-200 place-content-center header-height">
-        <h2 className="mx-auto">Select World</h2>
-        <input className="world-select mx-auto" type="text"></input>
+        <h2 className="mx-auto pb-2">Select World</h2>
+        <input className="world-select-text" type="text"></input>
       </div>
     );
 }
